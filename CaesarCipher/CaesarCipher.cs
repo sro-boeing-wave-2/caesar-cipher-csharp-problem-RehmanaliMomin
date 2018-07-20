588455c46fa96a651476f3e242a66c5a4dafc1d4
rehman <rehmanmomin@gmail.com>
2018-07-20 11:27:46 +0530

    solved the solution

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CaesarCipher
 {
@@ -7,7 +9,40 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string s = "";
+            int a;
+            int x;
+            StringBuilder sb = new StringBuilder();
+
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            string alphaCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Regex.IsMatch(text[i].ToString(), @"^[a-z]+$"))
+                {
+                    a = alpha.IndexOf(text[i]);
+                    x = (a + shiftKey) % 26;
+                    sb.Append(alpha[x]);
+                }
+                else if (Regex.IsMatch(text[i].ToString(), @"^[A-Z]+$"))
+                {
+                    a = alphaCaps.IndexOf(text[i]);
+                    x = (a + shiftKey) % 26;
+                    sb.Append(alphaCaps[x]);
+                }
+                else
+                {
+                    sb.Append(text[i]);
+                }
+
+
+            }
+
+
+
+            return sb.ToString();
+
         }
     }
 }
